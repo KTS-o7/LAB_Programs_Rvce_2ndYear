33f6e722560d17a4293d426f91f2a54fdd8ab329
KTS-o7 <teju2403@gmail.com>
2023-08-29 21:27:41 +0530

    Add Lab Program heapsorting

--- heapsort.c
+++ heapsort.c
@@ -2,44 +2,57 @@
 
 void heapify(int arr[], int n)
 {
-    int i,j,k,v,flag;
-    for(i=n/2;i>=1;i--)
+    int i, j, k, v, flag;
+    for (i = n / 2; i >= 1; i--)
     {
-        k=i;
-        v=arr[k];
-        flag=0;
-        while(!flag && 2*k<=n)
+        k = i;
+        v = arr[k];
+        flag = 0;
+        while (!flag && 2 * k <= n)
         {
-            j=2*k;
-            if(j<n)
+            j = 2 * k;
+            if (j < n)
             {
-                if(arr[j]<arr[j+1])
+                if (arr[j] < arr[j + 1])
                     j++;
             }
-            if(v>=arr[j])
-                flag=1;
+            if (v >= arr[j])
+                flag = 1;
             else
             {
-                arr[k]=arr[j];
-                k=j;
+                arr[k] = arr[j];
+                k = j;
             }
         }
-        arr[k]=v;
+        arr[k] = v;
+    }
+}
+
+void heapSort(int arr[], int n)
+{
+    int i, temp;
+    heapify(arr, n);
+    for (i = n; i >= 2; i--)
+    {
+        temp = arr[1];
+        arr[1] = arr[i];
+        arr[i] = temp;
+        heapify(arr, i - 1);
     }
 }
 
 int main()
 {
-    int arr[100],n,i;
+    int arr[100], n, i;
     printf("Enter the number of elements: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
     printf("Enter the elements: ");
-    for(i=1;i<=n;i++)
-        scanf("%d",&arr[i]);
-    heapify(arr,n);
+    for (i = 1; i <= n; i++)
+        scanf("%d", &arr[i]);
+    heapSort(arr, n);
     printf("The sorted array is: ");
-    for(i=1;i<=n;i++)
-        printf("%d ",arr[i]);
+    for (i = 1; i <= n; i++)
+        printf("%d ", arr[i]);
     printf("\n");
     return 0;
 }
